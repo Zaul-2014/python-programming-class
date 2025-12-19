c1e38032544309f8813d7907f26293164801bd45
zaul-2014 <zeenathsaee09@gmail.com>
2025-12-19 11:25:45 +0500

    Add files via upload
    dicts, tuples

---
+++ Dictionaries and Tuples/dicts.py
@@ -0,0 +1,20 @@
+Data = {'name': 'zaul', 'Age': 11}
+
+print(Data.get('name'))
+
+Data['Grade'] = '5B'
+
+print(Data)
+
+Data['Age'] = '12years'
+
+print(Data)
+
+Data.pop('Grade')
+
+print(Data)
+
+Data.clear()
+
+print(Data)
+
---
+++ Dictionaries and Tuples/tuples.py
@@ -0,0 +1,5 @@
+Data = ('zaul', 11, '5B')
+
+print(Data[0])
+
+print(Data[1:3])
